24a9358e082ab791c0fc0fb3c73df4a0d3a7beba
K.Varshitha Reddy <168292995+varshithareddy2006@users.noreply.github.com>
2025-06-25 00:33:47 +0530

    app.py

--- app.py
+++ app.py
@@ -34,49 +34,15 @@ def predict_rating(user_id, movie_id, k=30, threshold=0.2):
         except KeyError:
             continue
     return total_rating / total_sim if total_sim != 0 else 0
-def nid_movies(user_id):
-  user_id=int(user_id)
-  if user_id not in user_item_matrix.index:
-    print(f"User {user_id} not found in matrix.")
-    return 
-  id=[]
-  nid=[]
-  for m in movies.movieId:
-    try:
-      rating = user_item_matrix.at[user_id, m]
-      if not pd.isna(rating):
-        id.append(m)
-    except KeyError:
-      continue
-  for i in movies.movieId:
-    if i not in id:
-      nid.append(i)
-  return nid
+
 # Flask app
 app = Flask(__name__)
 
-@app.route('/recommend',methods=['GET'])
-def recommend():
-    recommend_movies=[]
-    user_id=request.args.get('user_id')
-    N= int(request.args.get('N', 10))
-    n=nid_movies(user_id)
-    if n is None:
-        return jsonify({"error": "User not found or no movies available for recommendation."}), 404
-    else:
-        for m in movies.movieId:
-            m=int(m)
-            if m in n:
-                rating= predict_rating(user_id, m)
-                if rating > 0:
-                    title_row = movies[movies['movieId'] == m]
-                    if not title_row.empty:
-                        title = title_row.iloc[0]['title']
-                        recommend_movies.append({
-                            'title': title,
-                            'predicted_rating': rating
-                        })
-    recommend_movies = sorted(recommend_movies, key=lambda x: x['predicted_rating'], reverse=True)[:N]
-    return jsonify(recommend_movies)
+@app.route('/predict', methods=['GET'])
+def predict():
+    user_id = request.args.get('user_id')
+    movie_id = request.args.get('movie_id')
+    rating = predict_rating(user_id, movie_id)
+    return jsonify({'user_id': user_id, 'movie_id': movie_id, 'predicted_rating': round(rating, 2)})
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
